f233f3ce325aef737244b30451c3b27d8deb2c27
Sandoche Adittane <sandoche@gmail.com>
2022-05-18 13:09:26 +0200

    Adding animation in the NearAuthGuard and protecting the other route, closes #37'

--- components/common/NearAuthenticationGuardWithLoginRedirection.tsx
+++ components/common/NearAuthenticationGuardWithLoginRedirection.tsx
@@ -1,12 +1,19 @@
 import { useRouter } from 'next/router';
+import { useWallet } from '@/modules/near-api-react/hooks/useWallet';
 import NearAuthenticationGuard from '@/modules/near-api-react/utilities/NearAuthenticationGuard';
+import LoadingAnimation from '@/components/common/LoadingAnimation';
 
 function NearAuthenticationGuardWithLoginRedirection({ children }: { children: JSX.Element }) {
   const router = useRouter();
+  const wallet = useWallet();
 
   const redirectionCallBack = () => {
     router.push('/login');
   };
+
+  if (!wallet) {
+    return <LoadingAnimation />;
+  }
 
   return <NearAuthenticationGuard loggedInCallback={redirectionCallBack}>{children}</NearAuthenticationGuard>;
 }
