571d2dc6af45f99f96e2ae710b3ce1fb630f2768
Sandoche Adittane <sandoche@gmail.com>
2022-05-18 10:25:20 +0200

    Improving signature saving management

--- components/common/NearAuthenticationGuardWithLoginRedirection.tsx
+++ components/common/NearAuthenticationGuardWithLoginRedirection.tsx
@@ -1,24 +1,8 @@
 import { useRouter } from 'next/router';
-import { useCookies } from 'react-cookie';
-import { useEffect } from 'react';
-import { useAccountSignature } from '@/hooks/useAccountSignature';
 import NearAuthenticationGuard from '@/modules/near-api-react/utilities/NearAuthenticationGuard';
-import { COOKIE_SIGNATURE_KEY, COOKIE_EXPIRACY_TIME } from '@/constants';
 
 function NearAuthenticationGuardWithLoginRedirection({ children }: { children: JSX.Element }) {
   const router = useRouter();
-  const [, setCookie] = useCookies([COOKIE_SIGNATURE_KEY]);
-  const apiSignature = useAccountSignature();
-
-  useEffect(() => {
-    if (apiSignature) {
-      setCookie(COOKIE_SIGNATURE_KEY, JSON.stringify(apiSignature), {
-        path: '/',
-        maxAge: COOKIE_EXPIRACY_TIME,
-        sameSite: true,
-      });
-    }
-  }, [apiSignature]);
 
   const redirectionCallBack = () => {
     router.push('/login');
--- hooks/useAccountSignature.ts
+++ hooks/useAccountSignature.ts
@@ -2,10 +2,13 @@ import type NearApiSignatureInterface from '@/types/NearApiSignatureInterface';
 import { useEffect, useState } from 'react';
 import { useSigner } from '@/modules/near-api-react/hooks/useSigner';
 import { useWallet } from '@/modules/near-api-react/hooks/useWallet';
+import { useCookies } from 'react-cookie';
+import { COOKIE_SIGNATURE_KEY, COOKIE_EXPIRACY_TIME } from '@/constants';
 
 export const useAccountSignature = () => {
   const [apiSignature, setApiSignature] = useState<NearApiSignatureInterface>();
   const { signStringMessage } = useSigner();
+  const [, setCookie] = useCookies([COOKIE_SIGNATURE_KEY]);
   const wallet = useWallet();
 
   useEffect(() => {
@@ -13,9 +16,17 @@ export const useAccountSignature = () => {
 
     if (accountId) {
       signStringMessage(accountId).then((signature) => {
-        setApiSignature({
+        const fullSignature = {
           signature,
           accountId,
+        };
+
+        setApiSignature(fullSignature);
+
+        setCookie(COOKIE_SIGNATURE_KEY, JSON.stringify(fullSignature), {
+          path: '/',
+          maxAge: COOKIE_EXPIRACY_TIME,
+          sameSite: true,
         });
       });
     }
