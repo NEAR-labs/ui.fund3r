c2baefc5c2b1f2b8aeb8248e6f8a8d58d7febaa1
Sandoche Adittane <sandoche@gmail.com>
2022-05-18 12:54:44 +0200

    Deleting useless line

--- pages/grants/index.tsx
+++ pages/grants/index.tsx
@@ -1,6 +1,5 @@
 import type { NextApiRequest } from 'next';
 import Head from 'next/head';
-import { Container } from '@mantine/core';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 import NearAuthenticationGuardWithLoginRedirection from '@/components/common/NearAuthenticationGuardWithLoginRedirection';
