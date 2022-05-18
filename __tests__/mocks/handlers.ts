eade8e74c803f681ac4909b17a31c3f3cfb539da
Sandoche Adittane <sandoche@gmail.com>
2022-05-18 11:31:50 +0200

    Mocking different ids for better testing

--- __tests__/mocks/handlers.ts
+++ __tests__/mocks/handlers.ts
@@ -5,13 +5,15 @@ const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_HOST;
 
 const getGrantsData = (accountId: string): GrantApplicationInterface[] => [{ id: 0, nearId: accountId }];
 
-const getGrantDataOne = (accountId: string): GrantApplicationInterface => {
-  return { id: 0, nearId: accountId };
+const getGrantData = (accountId: string, id: number): GrantApplicationInterface => {
+  return [
+    { id: 0, nearId: accountId },
+    {
+      id: 1,
+      nearId: accountId,
+    },
+  ][id];
 };
-
-// const getGrantDataTwo = (accountId: string): GrantApplicationInterface => {
-//   return { id: 1, nearId: accountId };
-// };
 
 export const handlers = [
   // The backend will verify the signature following this https://stackoverflow.com/questions/61982163/jwt-authentication-for-near-protocol-in-a-python-backend
@@ -22,6 +24,6 @@ export const handlers = [
   }),
 
   rest.get<never, any>(`${BASE_URL}/grants/:accountId-:id`, (_req, res, ctx) => {
-    return res(ctx.json(getGrantDataOne(_req.headers.get('X-NEAR-ACCOUNT-ID') as string)));
+    return res(ctx.json(getGrantData(_req.headers.get('X-NEAR-ACCOUNT-ID') as string, parseInt(_req.params.id))));
   }),
 ];
