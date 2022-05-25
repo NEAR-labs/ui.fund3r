1b32cb4cdc9c8de19b64c02296686bcbcf0ca519
Sandoche Adittane <sandoche@gmail.com>
2022-05-25 10:43:29 +0200

    Handling select fields

--- components/auto-form/GenericField.tsx
+++ components/auto-form/GenericField.tsx
@@ -1,7 +1,11 @@
-import { NumberInput, TextInput } from '@mantine/core';
+import { NumberInput, TextInput, Select } from '@mantine/core';
+import { useTranslation } from 'next-i18next';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line max-lines-per-function
 function GenericField(props: any) {
+  const { t } = useTranslation('grant');
+
   const { zodTypeDef, ...otherProps } = props;
   const typeName = zodTypeDef.innerType || zodTypeDef.typeName;
   const required = zodTypeDef.typeName !== 'ZodOptional';
@@ -10,6 +14,8 @@ function GenericField(props: any) {
     required,
   };
 
+  console.log(zodTypeDef);
+
   switch (typeName) {
     case 'ZodString':
       return <TextInput {...sharedProps} {...otherProps} />;
@@ -17,6 +23,16 @@ function GenericField(props: any) {
 
     case 'ZodNumber':
       return <NumberInput {...sharedProps} {...otherProps} />;
+      break;
+
+    case 'ZodNativeEnum':
+      // eslint-disable-next-line no-case-declarations
+      const data = Object.keys(zodTypeDef.values).map((key) => ({
+        label: t(`form.labels.${zodTypeDef.values[key]}`),
+        value: zodTypeDef.values[key],
+      }));
+
+      return <Select {...sharedProps} {...otherProps} data={data} />;
       break;
 
     default:
