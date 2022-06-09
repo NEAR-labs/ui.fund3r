861d42130a3b4c3ff22fad578f4812356b2a9887
Sandoche Adittane <sandoche@protonmail.com>
2022-06-09 13:24:11 +0100

    Work in progress kycdao sdk react

--- modules/kycdao-sdk-react/providers/KycDaoProvider.ts
+++ modules/kycdao-sdk-react/providers/KycDaoProvider.ts
@@ -1,1 +1,23 @@
+import type { SdkConfiguration } from '@kycdao/kycdao-sdk';
+import { useEffect, useState } from 'react';
+import { KycDao } from '@kycdao/kycdao-sdk';
 
+import getConfig from '../config';
+import KycDaoContext from '../context/KycDaoContext';
+
+const KycDaoProvider = ({ children, networkId = 'tesnet', config }: { children: JSX.Element; networkId: string; config: SdkConfiguration }) => {
+  const [kycDao, setKycDao] = useState<KycDao | null>(null);
+
+  useEffect(() => {
+    const defaultConfig = getConfig(networkId);
+    const kycDaoInstance = new KycDao({
+      ...defaultConfig,
+      ...config,
+    });
+    setKycDao(kycDaoInstance);
+  }, [networkId, config]);
+
+  return <KycDaoContext.Provider value={kycDao}>{children}</KycDaoContext.Provider>;
+};
+
+export default KycDaoProvider;
