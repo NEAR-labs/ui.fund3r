c84e36c5aa2539fb864ec2f939a4f6bdeb9c80c9
Sandoche Adittane <sandoche@protonmail.com>
2022-06-16 15:15:19 +0100

    Handling errors properly

--- pages/grants/index.tsx
+++ pages/grants/index.tsx
@@ -9,6 +9,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import LoadingAnimation from '@/components/common/LoadingAnimation';
 import NearAuthenticationGuardWithLoginRedirection from '@/components/common/NearAuthenticationGuardWithLoginRedirection';
+import Error401 from '@/components/errors/401';
 import Error500 from '@/components/errors/500';
 import { COOKIE_SIGNATURE_KEY } from '@/constants';
 import useAccountSignature from '@/hooks/useAccountSignature';
@@ -16,7 +17,7 @@ import DefaultLayout from '@/layouts/default';
 import { getAllGrantApplicationsOfUser } from '@/services/apiService';
 import parseCookies from '@/utilities/parseCookies';
 
-function Grants() {
+function Grants({ error }: { error: number }) {
   const router = useRouter();
   const { t } = useTranslation('grants');
   const apiSignature = useAccountSignature();
@@ -29,6 +30,19 @@ function Grants() {
       router.push(`/grants/${grant.nearId}-${grant.id}`);
     }
   }, [data, router]);
+
+  if (error === 401) {
+    return (
+      <DefaultLayout>
+        <>
+          <Head>
+            <title>{t('title')}</title>
+          </Head>
+          <Error401 />
+        </>
+      </DefaultLayout>
+    );
+  }
 
   return (
     <DefaultLayout>
@@ -51,13 +65,28 @@ export async function getServerSideProps({ req, locale }: { req: NextApiRequest;
   const data = parseCookies(req);
   const apiSignature = data[COOKIE_SIGNATURE_KEY] ? JSON.parse(data[COOKIE_SIGNATURE_KEY]) : null;
 
-  await queryClient.prefetchQuery(['grants', apiSignature], () => getAllGrantApplicationsOfUser(apiSignature));
-  const dehydratedState = dehydrate(queryClient);
+  try {
+    await queryClient.fetchQuery(['grants', apiSignature], () => getAllGrantApplicationsOfUser(apiSignature));
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    if (e.response.status === 401) {
+      return {
+        props: {
+          ...(await serverSideTranslations(locale, ['common', 'grant'])),
+          error: 401,
+        },
+      };
+    }
+
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
       ...(await serverSideTranslations(locale, ['common', 'grants'])),
-      dehydratedState,
+      dehydratedState: dehydrate(queryClient),
     },
   };
 }
