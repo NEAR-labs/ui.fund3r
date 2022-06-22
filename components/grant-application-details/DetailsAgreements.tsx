611cbc18bbcd8f03f35509d549b3e57254dcfbf8
Sandoche Adittane <sandoche@gmail.com>
2022-06-22 10:13:47 +0200

    Refactoring agreement download

--- components/grant-application-details/DetailsAgreements.tsx
+++ components/grant-application-details/DetailsAgreements.tsx
@@ -1,46 +1,19 @@
 import { Button, Title } from '@mantine/core';
-import axios from 'axios';
 import { useTranslation } from 'next-i18next';
 
 import useAccountSignature from '@/hooks/useAccountSignature';
-
-// import { getFileDownloadUrl } from '@/services/helloSignService';
+import { downloadFile } from '@/services/helloSignService';
 
 function DetailsAgreements({ grantId }: { grantId: number | undefined }) {
   const { t } = useTranslation('grant');
-
-  const API_HOST = process.env.NEXT_PUBLIC_BACKEND_HOST;
   const apiSignature = useAccountSignature();
-
-  const downloadFile = () => {
-    if (!apiSignature) {
-      return;
-    }
-
-    axios({
-      url: `${API_HOST}/api/v1/grants/${grantId}/agreement`,
-      method: 'GET',
-      responseType: 'blob',
-      headers: {
-        'X-NEAR-ACCOUNT-ID': apiSignature.accountId,
-        'X-NEAR-SIGNATURE': JSON.stringify(apiSignature.signature),
-      },
-    }).then((response) => {
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', 'agreement.zip');
-      document.body.appendChild(link);
-      link.click();
-    });
-  };
 
   return (
     <>
       <Title order={3} mb="lg" mt="xl">
         {t('details.agreements.title')}
       </Title>
-      <Button variant="light" color="gray" onClick={downloadFile}>
+      <Button variant="light" color="gray" onClick={() => downloadFile(apiSignature, grantId)}>
         {t('details.agreements.filename')}
       </Button>
     </>
--- services/helloSignService.ts
+++ services/helloSignService.ts
@@ -1,10 +1,30 @@
-const getFileDownloadUrl = (helloSignSignatureRequestId: string | undefined) => {
-  if (!helloSignSignatureRequestId) {
-    return null;
+import axios from 'axios';
+import type NearApiSignatureInterface from 'types/NearApiSignatureInterface';
+
+const API_HOST = process.env.NEXT_PUBLIC_BACKEND_HOST;
+
+const downloadFile = (apiSignature: NearApiSignatureInterface | undefined, grantId: number | undefined) => {
+  if (!apiSignature || !grantId) {
+    return;
   }
 
-  return `https://app.hellosign.com/home/manage?guid=${helloSignSignatureRequestId}`;
+  axios({
+    url: `${API_HOST}/api/v1/grants/${grantId}/agreement`,
+    method: 'GET',
+    responseType: 'blob',
+    headers: {
+      'X-NEAR-ACCOUNT-ID': apiSignature.accountId,
+      'X-NEAR-SIGNATURE': JSON.stringify(apiSignature.signature),
+    },
+  }).then((response) => {
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'agreement.zip');
+    document.body.appendChild(link);
+    link.click();
+  });
 };
 
 // eslint-disable-next-line import/prefer-default-export
-export { getFileDownloadUrl };
+export { downloadFile };
