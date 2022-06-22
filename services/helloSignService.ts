2796dd7c313ae4718849e84b1d1b2913eff5adab
Sandoche Adittane <sandoche@gmail.com>
2022-06-22 10:17:35 +0200

    Adding loading animation

--- services/helloSignService.ts
+++ services/helloSignService.ts
@@ -3,12 +3,12 @@ import type NearApiSignatureInterface from 'types/NearApiSignatureInterface';
 
 const API_HOST = process.env.NEXT_PUBLIC_BACKEND_HOST;
 
-const downloadFile = (apiSignature: NearApiSignatureInterface | undefined, grantId: number | undefined) => {
+const downloadFile = async (apiSignature: NearApiSignatureInterface | undefined, grantId: number | undefined) => {
   if (!apiSignature || !grantId) {
     return;
   }
 
-  axios({
+  const response = await axios({
     url: `${API_HOST}/api/v1/grants/${grantId}/agreement`,
     method: 'GET',
     responseType: 'blob',
@@ -16,14 +16,14 @@ const downloadFile = (apiSignature: NearApiSignatureInterface | undefined, grant
       'X-NEAR-ACCOUNT-ID': apiSignature.accountId,
       'X-NEAR-SIGNATURE': JSON.stringify(apiSignature.signature),
     },
-  }).then((response) => {
-    const url = window.URL.createObjectURL(new Blob([response.data]));
-    const link = document.createElement('a');
-    link.href = url;
-    link.setAttribute('download', 'agreement.zip');
-    document.body.appendChild(link);
-    link.click();
   });
+
+  const url = window.URL.createObjectURL(new Blob([response.data]));
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', 'agreement.zip');
+  document.body.appendChild(link);
+  link.click();
 };
 
 // eslint-disable-next-line import/prefer-default-export
